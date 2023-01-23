e70e4020a929d3517cd1048cf41fe5cd838100d3
FrancoTalles <francotallesextreme@gmail.com>
2023-01-22 22:40:48 -0400

    Add Registration Router

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import express from "express";
 import cors from "cors";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
+import joi from "joi";
+import bcrypt from "bcrypt";
 
 // Iniciando Configurações
 
@@ -22,15 +24,76 @@ try {
   await mongoClient.connect();
   console.log("MongoDB Connected!");
 } catch (error) {
-    res.status(500).send(error.message);
+  res.status(500).send(error.message);
 }
 
 db = mongoClient.db();
 
 //Rotas
 
-app.post("/", async (req, res) => {
-    res.status(200).send("Rota Login funcionando");
+app.post("/login", async (req, res) => {
+  const user = req.body;
+
+  const userSchema = joi.object({
+    email: joi.string().email().required(),
+    password: joi.string().required(),
+  });
+
+  const validation = userSchema.validate(user, { abortEarly: false });
+
+  if (validation.error) {
+    const errors = validation.error.details.map((detail) => detail.message);
+    return res.status(422).send(errors);
+  }
+
+  res.status(200).send("Rota Login funcionando");
+});
+
+app.post("/cadastro", async (req, res) => {
+  const userCadastro = req.body;
+
+  const cadastroSchema = joi.object({
+    name: joi.string().required(),
+    email: joi.string().email().required(),
+    password: joi.string().min(3).max(15).required().label("Password"),
+    confirmPassword: joi
+      .any()
+      .equal(joi.ref("password"))
+      .required()
+      .label("Confirma password")
+      .options({ messages: { "any.only": "{{#label}} does not match" } }),
+  });
+
+  try {
+    const validation = cadastroSchema.validate(userCadastro, {
+      abortEarly: false,
+    });
+
+    if (validation.error) {
+      const errors = validation.error.details.map((detail) => detail.message);
+      return res.status(422).send(errors);
+    }
+
+    const resp = await db.collection("users").findOne({
+      email: userCadastro.email
+    });
+
+    if (resp) {
+      return res.status(400).send("Email já cadastrado");
+    }
+
+    const senhaCriptografada = bcrypt.hashSync(userCadastro.password, 10);
+
+    delete userCadastro.confirmPassword;
+
+    await db.collection("users").insertOne({
+      ...userCadastro, password: senhaCriptografada
+    });
+
+    return res.sendStatus(200);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 });
 
 // Port
@@ -38,5 +101,3 @@ app.post("/", async (req, res) => {
 const PORT = 5000;
 
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
-
-
